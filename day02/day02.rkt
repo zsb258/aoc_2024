29b965711992cc3201b2189e950e230a6617baf7
zsb258 <zhuang.shengbin@u.nus.edu>
2024-12-03 19:06:35 +0800

    day02 refactor

--- day02/day02.rkt
+++ day02/day02.rkt
@@ -9,51 +9,42 @@
        (string-split (file->string fname) "\n")))
 
 (define (pairwise-diffs lst)
-  (define v (list->vector lst))
-  (define n (vector-length v))
-  (vector->list
-   (vector-map -
-               (vector-copy v 0 (sub1 n))
-               (vector-copy v 1 n))))
+  (define n (sub1 (length lst)))
+  (map - (take lst n) (take-right lst n)))
 
 (define (same-sign? diffs)
   (or (andmap positive-integer? diffs)
-      (andmap negative-integer? diffs))
-  #||#)
+      (andmap negative-integer? diffs)))
 
-; 1 <= x <= 3
+; 1 <= |x| <= 3
 (define (safe-diff? x)
-  ((compose (lambda (x) (and (>= x 1) (<= x 3)))
-            abs)
-   x))
+  ((compose (curryr member (range 1 4)) abs) x))
 
 (define (safe-diffs? diffs)
   (andmap safe-diff? diffs))
 
 (define (safe-1? lst)
   (define diffs (pairwise-diffs lst))
-  (and (same-sign? diffs) (safe-diffs? diffs))
-  #||#)
+  (and (same-sign? diffs) (safe-diffs? diffs)))
 
 (define (remove-at lst i)
   (append (take lst i)
-          (take-right lst
-                      (sub1 (- (length lst) i)))))
+          (take-right lst (- (length lst) i 1)))
+  #||#)
 
 (define (safe-2? lst)
   (or (safe-1? lst)
       (ormap safe-1?
-             (map (lambda (i) (remove-at lst i))
-                  (range (length lst)))))
+             (map (curry remove-at lst)
+                  (range (length lst))))
+      #||#)
   #||#)
 
 (define (part1 input)
-  (count safe-1? input)
-  #||#)
+  (count safe-1? input))
 
 (define (part2 input)
-  (count safe-2? input)
-  #||#)
+  (count safe-2? input))
 
 (module+ test
   (require rackunit)
